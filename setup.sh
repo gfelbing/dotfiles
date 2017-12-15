1169a917e48cb6824eeccaa737066e7ab46cc4df
Georg Felbinger <gfelbing@github.com>
2017-12-15 15:18:19 +0100

    setup links files now, add vundle

--- setup.sh
+++ setup.sh
@@ -3,6 +3,20 @@
 set -e
 set -x
 
+BASEDIR=$(realpath $(dirname $0))
+
 pacman -S zsh gvim awesome ttf-hack termite xscreensaver
 chsh -s /usr/bin/zsh
 
+FILES=".config/awesome \
+        .config/termite \
+        .env \
+        .oh-my-zsh \
+        .vim/bundle/Vundle.vim \
+        .vimrc \
+        .zsh_aliases \
+        .zshrc"
+
+for FILE in $FILES; do
+    echo "ln -s $BASEDIR/$FILE ~/$FILE"
+done
