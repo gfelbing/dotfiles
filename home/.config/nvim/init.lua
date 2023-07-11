446b5d170556d35a2e05b91c19b9d2a35626df73
Georg Welzel <georg.welzel@freiheit.com>
2023-07-11 16:39:21 +0200

    mv coverage setup to init

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -51,7 +51,26 @@ require('packer').startup(function()
     "andythigpen/nvim-coverage",
     requires = "nvim-lua/plenary.nvim",
     config = function()
-      require("user.coverage")
+      require("coverage").setup({
+        commands = true, -- create commands
+        highlights = {
+          -- customize highlight groups created by the plugin
+          covered = { fg = "#007804" },   -- supports style, fg, bg, sp (see :h highlight-gui)
+          uncovered = { fg = "#eb1f00" },
+        },
+        signs = {
+          -- use your own highlight groups or text markers
+          covered = { hl = "CoverageCovered", text = "✓" },
+          uncovered = { hl = "CoverageUncovered", text = "✗" },
+        },
+        summary = {
+          -- customize the summary pop-up
+          min_coverage = 80.0,      -- minimum coverage threshold (used for highlighting)
+        },
+        lang = {
+          -- customize language specific settings
+        },
+      })
     end,
   })
 end)
--- home/.config/nvim/lua/config-coverage.lua
+++ home/.config/nvim/lua/config-coverage.lua
@@ -2,27 +2,6 @@ local coverage = require("coverage")
 local keys = require("config-keys")
 local opts = { noremap = true, silent = true }
 
-coverage.setup({
-	commands = true, -- create commands
-	highlights = {
-		-- customize highlight groups created by the plugin
-		covered = { fg = "#007804" },   -- supports style, fg, bg, sp (see :h highlight-gui)
-		uncovered = { fg = "#eb1f00" },
-	},
-	signs = {
-		-- use your own highlight groups or text markers
-		covered = { hl = "CoverageCovered", text = "✓" },
-		uncovered = { hl = "CoverageUncovered", text = "✗" },
-	},
-	summary = {
-		-- customize the summary pop-up
-		min_coverage = 80.0,      -- minimum coverage threshold (used for highlighting)
-	},
-	lang = {
-		-- customize language specific settings
-	},
-})
-
 vim.keymap.set('n', keys.Testing.Coverage, function ()
   coverage.load(false)
   coverage.toggle()
